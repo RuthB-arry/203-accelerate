df223a3b3f106257d665c18ab35d863701bc5e1e
ruth_barry <ruthbarry01@gmail.com>
2021-01-29 19:16:19 +0100

    case studies on homepage and more

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying the archive of case studies
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -14,39 +14,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
+<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); 
 				$services = get_field ('services');
-				$client = get_field ('client');
-				$link = get_field ('site_link'); 
 				$image_1 = get_field ('image_1');
-				$image_2 = get_field ('image_2');
-				$image_3 = get_field ('image_3');
-				$size = "medium";
+				$size = "full";
 				?>
 
 				<article class="case-study">
 					<aside class="case-study-sidebar">
 						<h2><?php the_title(); ?></h2>
+							<a href="<?php the_permalink(); ?>">
 						<h4><?php echo $services; ?></h4>
-						<h6>Client: <?php echo $client; ?></h6>
-						
-						<?php the_content(); ?>
+						<?php the_excerpt(); ?>
 							<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
 					</aside>
-						<div class="single-case-study-images">
+						<div class="archive-case-study-images">
+							<a href="<?php the_permalink(); ?>">
 						<?php if($image_1){
 							echo wp_get_attachment_image( $image_1, $size );
 						} ?>
 
-						<?php if($image_2) { 
-							echo wp_get_attachment_image( $image_2, $size ); 
-						} ?>
-						
-						<?php if($image_3) {
-							echo wp_get_attachment_image( $image_3, $size ); 
-						 } ?>
 						</div>
             <?php endwhile; // end of the loop. ?>
     	
@@ -55,8 +44,8 @@ get_header(); ?>
     
 </div>    
 
+		
+
 	</div><!-- #primary -->
-	<nav id="navigation" class="container">
-	<div class="left"><a href="<?php echo site_url('/blog/') ?>">&larr; <span>Back to work</span></a></div>
-</nav>
+
 <?php get_footer(); ?>
